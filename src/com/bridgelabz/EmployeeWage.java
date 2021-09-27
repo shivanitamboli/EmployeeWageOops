8f7cbb8758c4cc2b0b552cf122285a853c9b5c52
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-27 14:43:04 +0530

    Added : Employee wage program

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,17 +3,23 @@ package com.bridgelabz;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-	        //Constants
-	        int IS_PRESENT = 1;
-	        System.out.println("Welcome to Employee Wage");
-	        double empCheck = Math.floor(Math.random()* 10) %2;
-	        // Using random class to Get random number
-	        if (empCheck == IS_PRESENT){ //echEmp get 1 when first condition is true and showing employee present
-	            System.out.println("Employee is Present");
-	        }else {
-	            System.out.println("Employee is Absent");
-	        }
+        //Constants
+        int IS_FULL_TIME = 1;
+        int  ENP_RATE_PER_HOUR = 20;
+        // Variables
+        int emHrs = 0;
+        int empWage = 0;
+        System.out.println("Welcome to Employee Wage");
+        // computation
+        double empCheck = Math.floor(Math.random()* 10) %2;
+        // Using random class to Get random number
+        if (empCheck == IS_FULL_TIME){ //echEmp get 1 when first condition is true and showing employee present
+            emHrs = 8;
+        }else {
+            emHrs = 0;
+        }
+        empWage = emHrs * ENP_RATE_PER_HOUR;
+        System.out.println("Employee Wage " + empWage);
 
-	}
-
+}
 }
