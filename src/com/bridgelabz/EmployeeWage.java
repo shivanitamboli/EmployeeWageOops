637a7ac767e7fd76f752226528e9c6f97f3365fb
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-27 15:08:49 +0530

    Added : employee wage using class and methods

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -9,6 +9,9 @@ public class EmployeeWage {
     public static final int MAX_HRS_IN_MONTH = 100;
 
     public static void main(String[] args) {
+        computeWage();
+    }
+    public static int computeWage(){
         System.out.println("Welcome to Employee Wage Computation");
         int empHrs = 0;
         int totalEmpHrs = 0;
@@ -33,6 +36,7 @@ public class EmployeeWage {
         }
         int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println("Total Emp Wage: " + totalEmpWage);
+        return totalEmpWage;
     }
 }
 
