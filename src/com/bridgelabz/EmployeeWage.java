8e45ffda7f3dc1199ba424f2e5d063b9e804959a
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-27 15:31:33 +0530

    Added : program by interface approach

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,23 +1,23 @@
 package com.bridgelabz;
 
-public class EmployeeWage {
+public class EmployeeWage implements ComputeWage{
     public static final int isPartTime=0;
     public static final int isFullTime=1;
     private int numOfCompany;
     private CompanyEmpWage empWageArray[];
-
     public EmployeeWage() {
         empWageArray=new CompanyEmpWage[5];
     }
-    private void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maximumHoursPerMonth) {
+    public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maximumHoursPerMonth) {
         empWageArray[numOfCompany]=new CompanyEmpWage(company,empRatePerHour,numOfWorkingDays,maximumHoursPerMonth);
         numOfCompany++;
     }
-    private void computeEmpWage() {
+    public void computeEmpWage() {
         for(int i=0;i<numOfCompany;i++) {
             empWageArray[i].setTotalEmpWage(this.computeEmpWage(empWageArray[i]));
             System.out.println(empWageArray[i]);
         }
+
     }
     private int computeEmpWage(CompanyEmpWage companyEmpWage) {
         int empHours=0, totalEmpHours=0, totalWorkingDays=0;
@@ -37,13 +37,11 @@ public class EmployeeWage {
             totalWorkingDays+=1;
         }
         return totalEmpHours * companyEmpWage.empRatePerHour;
-
     }
     public static void main(String args[]) {
         EmployeeWage employeeWage=new EmployeeWage();
-        employeeWage.addCompanyEmpWage("Dmart", 40, 20, 83);
-        employeeWage.addCompanyEmpWage("Reliance", 50, 21, 125);
+        employeeWage.addCompanyEmpWage("Dmart", 17, 20, 125);
+        employeeWage.addCompanyEmpWage("Reliance", 25, 21, 70);
         employeeWage.computeEmpWage();
-}
-
+    }
 }
