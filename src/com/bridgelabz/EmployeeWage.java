8cd0464432f641c802e91b74c3d19f2ecf683a72
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-27 14:47:06 +0530

    Added : Employee part time program

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,22 +4,25 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
         //Constants
-        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int  ENP_RATE_PER_HOUR = 20;
         // Variables
         int emHrs = 0;
         int empWage = 0;
         System.out.println("Welcome to Employee Wage");
         // computation
-        double empCheck = Math.floor(Math.random()* 10) %2;
+        double empCheck = Math.floor(Math.random()* 10) % 3;
         // Using random class to Get random number
-        if (empCheck == IS_FULL_TIME){ //echEmp get 1 when first condition is true and showing employee present
+        if (empCheck == IS_PART_TIME){
+            emHrs = 4;
+        }else if (empCheck == IS_FULL_TIME){
             emHrs = 8;
         }else {
             emHrs = 0;
         }
         empWage = emHrs * ENP_RATE_PER_HOUR;
         System.out.println("Employee Wage " + empWage);
+    }
+}
 
-}
-}
